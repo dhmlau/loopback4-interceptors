b3a72f86a9ad35b1eea77d4b094830c01d7cad7c
Diana Lau <dhmlau@ca.ibm.com>
2019-07-11 17:37:13 -0400

    First commit

---
+++ src/models/order.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {}})
+export class Order extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  orderNum?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  customerNum: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  customerEmail: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+
+  constructor(data?: Partial<Order>) {
+    super(data);
+  }
+}
+
+export interface OrderRelations {
+  // describe navigational properties here
+}
+
+export type OrderWithRelations = Order & OrderRelations;
